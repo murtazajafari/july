e9be75d5fd047ed6e5ef572065a7506c1f32fefb
Murtaza JAFARI <murtazajafari@Murtazas-MacBook-Pro.local>
2022-08-06 15:01:11 +1000

    working with react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {useReducer} from 'react'
 import './App.css';
 
+
+const reducer = (state,action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return {count: state.count + 1, showtext: state.showtext}
+        case "TOGGLETEXT":
+            return {count: state.count, showtext: !state.showtext}
+        default:
+            return {count: state.count, showtext: state.showtext}
+    }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [state, dispatch] = useReducer(
+        reducer,
+        {
+            count: 0,
+            showtext: true
+        }
+    
+    )
+
+    return (
+        <div className="App">
+
+            <h2>{state.count}</h2>
+
+            <button onClick={()=> {
+                dispatch({type: "INCREMENT"})
+                // dispatch({type: "TOGGLETEXT"})
+            }}>Increase</button>
+            
+
+            <p>{state.showtext ? 'True' : 'False'}</p>
+        </div>
+    );
 }
 
 export default App;
