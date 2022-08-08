8ca86f3356c16861902ba4312f508b5b296151c7
Murtaza JAFARI <murtazajafari@Murtazas-MacBook-Pro.local>
2022-08-08 10:14:25 +1000

    working with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
 import {useReducer} from 'react'
 import './App.css';
 
-
-const reducer = (state,action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            return {count: state.count + 1, showtext: state.showtext}
-        case "TOGGLETEXT":
-            return {count: state.count, showtext: !state.showtext}
-        default:
-            return {count: state.count, showtext: state.showtext}
+const todosArray = [
+    {
+        id: 1,
+        title: 'Todo 1',
+        complete: true,
+    },
+    {
+        id: 2,
+        title: 'Todo 2',
+        complete: false,
+    },
+    {
+        id: 3,
+        title: 'Todo 3',
+        complete: false,
     }
+]
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "COMPLETE":
+            return state.map((todo)=>{
+                if(todo.id === action.id) {
+                    return {...todo, complete: !todo.complete}
+                } else {
+                    return todo
+                }
+            })
+    
+        default:
+            return state
+    }
+
 }
 function App() {
+
+    const [todos, dispatch] = useReducer(reducer, todosArray)
     
-    const [state, dispatch] = useReducer(
-        reducer,
-        {
-            count: 0,
-            showtext: true
-        }
-    
-    )
+    const handleComplete = (todo) => {
+        dispatch({type:"COMPLETE", id: todo.id})
+    }
 
     return (
         <div className="App">
 
-            <h2>{state.count}</h2>
-
-            <button onClick={()=> {
-                dispatch({type: "INCREMENT"})
-                // dispatch({type: "TOGGLETEXT"})
-            }}>Increase</button>
-            
-
-            <p>{state.showtext ? 'True' : 'False'}</p>
+            {
+                todos.map((todo) => (
+                    <div key={todo.id}>
+                        <label>
+                            <input 
+                                type="checkbox"
+                                checked={todo.complete}
+                                onChange={()=>{handleComplete(todo)}}
+                            
+                            />
+                            {todo.title}
+                        </label>
+                    </div>
+                ))
+            }
         </div>
     );
 }
