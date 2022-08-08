44ac0a9e240e6fdac8c2d52cee780a5f2807eb64
Murtaza JAFARI <murtazajafari@Murtazas-MacBook-Pro.local>
2022-08-08 14:08:47 +1000

    reducer() with todo app

--- src/App.js
+++ src/App.js
@@ -1,67 +1,142 @@
-import {useReducer} from 'react'
+import {useReducer, useState} from 'react'
 import './App.css';
 
-const todosArray = [
-    {
-        id: 1,
-        title: 'Todo 1',
-        complete: true,
-    },
-    {
-        id: 2,
-        title: 'Todo 2',
-        complete: false,
-    },
-    {
-        id: 3,
-        title: 'Todo 3',
-        complete: false,
-    }
-]
-
+const todosArray = []
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case "COMPLETE":
+        case "ADD":
+            return [...state, {
+                id: state.length + 1,
+                title: action.title,
+                status: 'todo'
+            }]
+        case "DOING":
             return state.map((todo)=>{
-                if(todo.id === action.id) {
-                    return {...todo, complete: !todo.complete}
+                if(todo.id == action.id) {
+                    return {...todo, status: 'doing'}
                 } else {
                     return todo
                 }
             })
-    
+        case "DONE":
+            return state.map((todo)=>{
+                if(todo.id == action.id) {
+                    return {...todo, status: 'done'}
+                } else {
+                    return todo
+                }
+            })
         default:
             return state
     }
-
 }
+
 function App() {
 
+    const [inputValue, setInputValue] = useState();
+
     const [todos, dispatch] = useReducer(reducer, todosArray)
-    
-    const handleComplete = (todo) => {
-        dispatch({type:"COMPLETE", id: todo.id})
+
+    const inputHandler = (e) => {
+        setInputValue(e.target.value);
+        
+    }
+    const addTodo = (e) => {
+        e.preventDefault();
+        dispatch({type: "ADD" , title: inputValue})
+
+        // clear fields 
+        document.querySelector("form").reset();
+    }
+
+    const markDoing = (todo) => {
+        dispatch({type: "DOING" , id:todo.id})
+    }
+
+    const markDone = (todo) => {
+        dispatch({type: "DONE" , id:todo.id})
     }
 
     return (
         <div className="App">
 
-            {
-                todos.map((todo) => (
-                    <div key={todo.id}>
-                        <label>
-                            <input 
-                                type="checkbox"
-                                checked={todo.complete}
-                                onChange={()=>{handleComplete(todo)}}
-                            
-                            />
-                            {todo.title}
-                        </label>
-                    </div>
-                ))
-            }
+            <h3>Todo app</h3>
+            <form onSubmit={(e)=> { addTodo(e)}}>
+                <input type="text" onChange={(e) => {inputHandler(e)}}  />
+            </form>
+            <h3>Todo</h3>
+            <table border={2} style={{borderCollapse: 'collapse',}}>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Action</th>
+                    </tr>
+                    {
+                        todos.map((todo) => {
+                            if(todo.status=="todo"){
+                                return (
+                                    <tr key={todo.id} draggable="true">
+                                        <td>{todo.id}</td>
+                                        <td>{todo.title}</td>
+                                        <td><button onClick={()=>markDoing(todo)}>Mark Doing</button></td>
+                                    </tr>
+                                )
+                            }
+                        })
+                    }
+                </tbody>
+            </table>
+
+            <h3>Doing</h3>
+            <table border={2} style={{borderCollapse: 'collapse',}}>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Action</th>
+                    </tr>
+                    {
+                        todos.map((todo) => {
+                            if(todo.status=="doing"){
+                                return (
+                                    <tr key={todo.id} draggable="true">
+                                        <td>{todo.id}</td>
+                                        <td>{todo.title}</td>
+                                        <td><button onClick={()=>markDone(todo)}>Mark Done</button></td>
+                                    </tr>
+                                )
+                            }
+                        })
+                    }
+                </tbody>
+            </table>
+
+            <h3>Done</h3>
+            <table border={2} style={{borderCollapse: 'collapse',}}>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Action</th>
+                    </tr>
+                    {
+                        todos.map((todo) => {
+                            if(todo.status=="done"){
+                                return (
+                                    <tr key={todo.id} draggable="true">
+                                        <td>{todo.id}</td>
+                                        <td>{todo.title}</td>
+                                        <td>Completed</td>
+                                    </tr>
+                                )
+                            }
+                        })
+                    }
+                </tbody>
+            </table>
+            
         </div>
     );
 }
