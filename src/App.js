84f7d445911e032a224bab792a337fe51646a7bb
Murtaza JAFARI <murtazajafari@Murtazas-MacBook-Pro.local>
2022-08-12 13:45:11 +1000

    working with useref hook on currency app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef} from 'react'
 import './App.css';
 import axios from 'axios';
 
 
 function App() {
 
-    const [from, setFrom] = useState();
-    const [to, setTo] = useState();
-    const [q, setQ] = useState();
+    // const [from, setFrom] = useState();
+    // const [to, setTo] = useState();
+    // const [q, setQ] = useState();
     const [currency, setCurrency] = useState();
 
-    const inputHandler = (e) => {
-        if(e.target.name === 'from') {
-            setFrom(e.target.value)
-        } else if(e.target.name === 'to') {
-            setTo(e.target.value)
-        } else if(e.target.name === 'q') {
-            setQ(e.target.value)
-        } else {
-            return 
-        }
-    }
+    // const inputHandler = (e) => {
+    //     if(e.target.name === 'from') {
+    //         setFrom(e.target.value)
+    //     } else if(e.target.name === 'to') {
+    //         setTo(e.target.value)
+    //     } else if(e.target.name === 'q') {
+    //         setQ(e.target.value)
+    //     } else {
+    //         return 
+    //     }
+    // }
+
+    const refFrom =  useRef('0');
+    const refTo =  useRef('0');
+    const refQ =  useRef('0');
 
     useEffect(() => {
         axios.get('https://currency-exchange.p.rapidapi.com/exchange', 
             { 
                 params: {
-                    from, 
-                    to,
-                    q 
+                    from: refFrom.current.value, 
+                    to : refTo.current.value,
+                    q: refQ.current.value 
                 },
                 headers: {
                     'X-RapidAPI-Key': '3e546240a6msh98a99a615f5e055p152e6bjsn22100f02406d',
@@ -39,7 +43,7 @@ function App() {
         .then((res)=> {
             console.log(res)
 
-            setCurrency(res.data * q)
+            setCurrency(res.data * refQ.current.value)
 
             
         })
@@ -47,8 +51,12 @@ function App() {
             console.error(err);
         });
 
-    }, [q])
+    }, [currency])
     
+
+    const inputHandler = (e) => {
+        setCurrency(e.target.value)
+    }
 
     
     return (
@@ -57,15 +65,15 @@ function App() {
 
             <label>
                 <span>From:</span>
-                <input type="text" name='from' placeholder='AUD' value={from} onChange={(e) => inputHandler(e)} />
+                <input type="text" name='from' placeholder='AUD' ref={refFrom} />
             </label>
             <label>
                 <span>To:</span>
-                <input type="text" name='to' placeholder='PKR' value={to} onChange={(e) => inputHandler(e)} />
+                <input type="text" name='to' placeholder='PKR' ref={refTo} />
             </label>
             <label>
                 <span>$:</span>
-                <input type="text" name='q' placeholder='$' value={q} onChange={(e) => inputHandler(e)} />
+                <input type="text" name='q' onChange={(e) => inputHandler(e)} placeholder='23' ref={refQ} />
             </label>
             <table border={2} style={{borderCollapse: 'collapse',}}>
                 <thead>
@@ -77,8 +85,8 @@ function App() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>{from}</td>
-                        <td>{to}</td>
+                        <td>{refFrom.current.value}</td>
+                        <td>{refTo.current.value}</td>
                         <td>{currency}</td>
                     </tr>
                 </tbody>
