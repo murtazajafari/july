36a4934fe3fb116d26af22b930aedb758815d8a0
Murtaza JAFARI <murtazajafari@Murtazas-MacBook-Pro.local>
2022-08-18 12:34:28 +1000

    working with random user API

--- src/App.js
+++ src/App.js
@@ -1,96 +1,73 @@
 import {useEffect, useState, useRef} from 'react'
-import './App.css';
 import axios from 'axios';
 
 
-function App() {
 
-    // const [from, setFrom] = useState();
-    // const [to, setTo] = useState();
-    // const [q, setQ] = useState();
-    const [currency, setCurrency] = useState();
-
-    // const inputHandler = (e) => {
-    //     if(e.target.name === 'from') {
-    //         setFrom(e.target.value)
-    //     } else if(e.target.name === 'to') {
-    //         setTo(e.target.value)
-    //     } else if(e.target.name === 'q') {
-    //         setQ(e.target.value)
-    //     } else {
-    //         return 
-    //     }
-    // }
-
-    const refFrom =  useRef('0');
-    const refTo =  useRef('0');
-    const refQ =  useRef('0');
-
-    useEffect(() => {
-        axios.get('https://currency-exchange.p.rapidapi.com/exchange', 
-            { 
-                params: {
-                    from: refFrom.current.value, 
-                    to : refTo.current.value,
-                    q: refQ.current.value 
-                },
-                headers: {
-                    'X-RapidAPI-Key': '3e546240a6msh98a99a615f5e055p152e6bjsn22100f02406d',
-                    'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-                }
-            }
-        )
-        .then((res)=> {
-            console.log(res)
-
-            setCurrency(res.data * refQ.current.value)
-
-            
+const fetchAPI = (nextPageNumber) => {
+    return axios
+        .get(`https://randomuser.me/api/?page=${nextPageNumber}`)
+        .then(({data}) => {
+            return data
         })
-        .catch((err)=> {
-            console.error(err);
-        });
+        .catch((err) => {
+            console.error(err)
+        })
+}
 
-    }, [currency])
+const getFullUserName = (userInfo) => {
+    const {name : {first,last}} = userInfo;
+    return `${first} ${last}`
+}
+const getUserPicture = (userInfo) => {
+    const {picture : {large}} = userInfo;
+    return `${large}`
+}
+
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const [randomDataJSON, setRandomDataJSON] = useState('');
+    const [userInfos, setUserInfos] = useState('');
+    const [nextPageNumber, setNextPageNumber] = useState(1);
+
+    // const fetchNextUser = useRef(()=>{});
     
+    const fetchNextUser = () => {
 
-    const inputHandler = (e) => {
-        setCurrency(e.target.value)
+        fetchAPI(nextPageNumber).then((randomData)=>{
+            setRandomDataJSON(JSON.stringify(randomData, null, 2) || 'Not data found')
+            if(randomData === undefined) return
+            const newUserInfo = [
+                ...userInfos,
+                ...randomData.results
+            ]
+            setUserInfos(newUserInfo)
+            setNextPageNumber(randomData.info.page + 1)
+        });
     }
 
+    useEffect(() => {
+        fetchNextUser()
+    }, [])
     
-    return (
-        <div className="App">
-            
 
-            <label>
-                <span>From:</span>
-                <input type="text" name='from' placeholder='AUD' ref={refFrom} />
-            </label>
-            <label>
-                <span>To:</span>
-                <input type="text" name='to' placeholder='PKR' ref={refTo} />
-            </label>
-            <label>
-                <span>$:</span>
-                <input type="text" name='q' onChange={(e) => inputHandler(e)} placeholder='23' ref={refQ} />
-            </label>
-            <table border={2} style={{borderCollapse: 'collapse',}}>
-                <thead>
-                    <tr>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>$</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{refFrom.current.value}</td>
-                        <td>{refTo.current.value}</td>
-                        <td>{currency}</td>
-                    </tr>
-                </tbody>
-            </table>
+    return (
+        <div className="App" style={{padding: '30px'}}>
+            <h1>Practicing with react API</h1>
+            <p>{counter}</p>
+            <button onClick={() => {setCounter(counter + 1)}}>Increase</button>
+            <button onClick={fetchNextUser}>Fetch Next user</button>
+            {
+                userInfos && userInfos.map((userInfo, idx) => (
+                    <div key={idx}>
+                        <p>{getFullUserName(userInfo)}</p>
+                        <img src={getUserPicture(userInfo)} alt="" />
+                    </div>
+                ))
+            }
+
+            {/* <pre>{userInfos}</pre> */}
+            <pre>{randomDataJSON}</pre>
         </div>
     );
 }
